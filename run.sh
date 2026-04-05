4af1359bcaa10c39ad5f958a544006d89fce63b6
Jason De Lorme <delormej@gmail.com>
2026-04-05 11:49:05 -0400

    feat: add --host 0.0.0.0 to uvicorn command in run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-(cd frontend && npm run build) && cd backend && ./venv/bin/uvicorn app.main:app --reload --port 8080
+(cd frontend && npm run build) && cd backend && ./venv/bin/uvicorn app.main:app --reload --port 8080 --host 0.0.0.0
